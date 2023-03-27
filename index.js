3d2e6d60755f709bcc6633bb046f697ada6ca912
Stella Mazzillo <stellagress5@gmail.com>
2023-03-26 20:46:08 -0400

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solutions in this file
+
+
+
+const messages = []
+
+function writeCards(name, event){
+
+    for(let i=0 ; i<name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages
+
+}
+
+
+
+
+
+function countDown(integer){
+    while (integer >= 0){
+        console.log(integer--)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
